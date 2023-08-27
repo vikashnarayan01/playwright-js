2702b126ac639810519789fb8fbdded80451dd4b
Vikash Narayan <vinarayan@altimetrik.com>
2023-08-27 12:49:50 +0530

    updated config & package file

--- playwright.config.js
+++ playwright.config.js
@@ -1,31 +1,71 @@
-// @ts-check
 const { devices } = require('@playwright/test');
 
 const config = {
-  testDir: './tests',
-  retries :2,
-  
+  use: {
+    baseURL: process.env.URL,
+    headless: true, // false - headed : true - headless
+    ignoreHTTPSErrors: true,
+
+    screenshot: 'on',
+    video: {
+      mode: "on", // "retain-on-failure",
+      size: { width: 1920, height: 1080 }
+    },
+
+    viewport: { width: 1280, height: 720 },
+    trace: 'on',
+    snapshot: 'on'
+  },
+
+  //testDir: './tests',
+  retries: 1,
+  // control the workers from here
+  workers: 3,
+
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
-  expect: {
-  
-    timeout: 5000
-  },
-  
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
+  expect: { timeout: 5000 },
 
-    browserName : 'chromium',
-    headless : false,
-    screenshot : 'on',
-    trace : 'on',//off,on
-    
-    
-    
-  },
+  // Allure report
+  reporter: [
+    ['line'],
+    ['allure-playwright']],
 
+  projects: [
+    /* Test against desktop browsers */
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
+    /* Test against branded browsers. */
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' }, // or 'chrome-beta'
+    },
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' }, // or 'msedge-dev'
+    },
+
+    /* Test against mobile viewports. */
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
+  ],
 };
 
 module.exports = config;
